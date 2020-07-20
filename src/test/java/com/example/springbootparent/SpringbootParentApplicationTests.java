43888696e5ada21f38a7dfe216c65b1167c1c618
wangwc <wwc953@163.com>
2020-07-20 20:34:46 +0800

    init

---
+++ src/test/java/com/example/springbootparent/SpringbootParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
