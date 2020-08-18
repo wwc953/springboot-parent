aaad98b58c93bd314adbd6360d621825ec2d4f7a
wangwc <wwc953@163.com>
2020-08-18 09:39:19 +0800

    golang变量

---
+++ go/src/main/java/com/example/go/day1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var a int = 13
+
+func main() {
+	//a := 12
+	fmt.Println(a)
+
+	// 声明一个变量并初始化
+	var a1 = "RUNOOB"
+	fmt.Println(a1)
+
+	// 默认为0
+	var b int
+	fmt.Println(b)
+
+	// bool 默认值为 false
+	var c bool
+	fmt.Println(c)
+
+	//默认""
+	var str string
+	fmt.Println(str)
+}
