177fb1dafd0d40aca886bbdc8ad1c75e0106f04a
wangwc <wwc953@163.com>
2020-08-26 09:21:48 +0800

    通过ApplicationContextAware接口,获取Spring创建的ApplicationContext对象

---
+++ web/src/main/java/com/example/web/utils/SpringUtil.java
@@ -0,0 +1,39 @@
+package com.example.web.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/**
+ * @Description: TODO 获取Spring创建的ApplicationContext
+ * @author: wangwc
+ * @date: 2020/8/26 9:15
+ */
+@Component
+public class SpringUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtil.applicationContext == null) {
+            SpringUtil.applicationContext = applicationContext;
+        }
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return SpringUtil.applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        Objects.requireNonNull(name);
+        return getApplicationContext().getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+}
