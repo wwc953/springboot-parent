bcc8eaba17ff4a51563f3c4cd56dec470643190a
wangwc <wwc953@163.com>
2020-08-26 14:15:37 +0800

    newCachedThreadPool,批量执行invokeAll

--- web/src/main/java/com/example/web/controller/CachedThreadPoolController.java
+++ web/src/main/java/com/example/web/controller/CachedThreadPoolController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * @Description: TODO
@@ -22,24 +25,44 @@ public class CachedThreadPoolController {
     private ExecutorService cachedThreadPool = Executors.newCachedThreadPool(new GroupThreadFactory("cache"));
 
     @GetMapping("/cache/{sum}")
-    public void cache(@PathVariable Integer sum) {
+    public String cache(@PathVariable Integer sum) throws ExecutionException, InterruptedException {
         ArrayList<MyCallable> taskList = new ArrayList<MyCallable>();
         for (int i = 0; i < sum; i++) {
             taskList.add(new MyCallable(i));
         }
-        try {
-            long start = System.currentTimeMillis();
-            List<Future<String>> futures = cachedThreadPool.invokeAll(taskList);
-            long end1 = start - System.currentTimeMillis();
+        long start = System.currentTimeMillis();
+        //批量任务 不限时
+        List<Future<String>> futures = cachedThreadPool.invokeAll(taskList);
+        long end1 = start - System.currentTimeMillis();
 
-            for (int i = 0; i < futures.size(); i++) {
-                System.out.println(futures.get(i).get());
+//        for (int i = 0; i < futures.size(); i++) {
+//            System.out.println(futures.get(i).get());
+//        }
+
+//        futures.stream().forEach(v -> {
+//            try {
+//                System.out.println(v.get());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
+
+        long end2 = start - System.currentTimeMillis();
+
+        System.out.println("任务执行时间：" + end1);
+        System.out.println("任务结果展示时间：" + end2);
+
+
+        String result = futures.stream().map(v -> {
+            try {
+                return v.get();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            long end2 = start - System.currentTimeMillis();
-            System.out.println("任务执行时间：" + end1);
-            System.out.println("任务结果展示时间：" + end2);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
+            return "-9999";
+        }).collect(Collectors.toList()).toString();
+        System.out.println(result);
+
+        return "success";
     }
 }
