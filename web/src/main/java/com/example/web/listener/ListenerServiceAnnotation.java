b1b58117ab026af034db69e7315287bdb5b38ed9
wangwc <wwc953@163.com>
2020-09-08 17:29:53 +0800

    update

--- web/src/main/java/com/example/web/listener/ListenerServiceAnnotation.java
+++ web/src/main/java/com/example/web/listener/ListenerServiceAnnotation.java
@@ -8,6 +8,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.concurrent.CompletableFuture;
+
 @Component
 public class ListenerServiceAnnotation {
 
@@ -20,13 +22,31 @@ public class ListenerServiceAnnotation {
         String planNo = redisEvent.getMessage();
         System.out.println("Annotation接受到RedisEvent事件,message:" + planNo);
         System.out.println(Thread.currentThread().getName() + " --------------begin---" + planNo);
-        try {
-            String res = helloService.sayHello();
-            System.out.println(res);
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            String res = helloService.sayHello();
+//            System.out.println(res);
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        long begin = System.currentTimeMillis();
+
+        CompletableFuture<String> fu1 = CompletableFuture.supplyAsync(() -> {
+            return helloService.sayHello();
+        });
+        CompletableFuture<String> fu2 = CompletableFuture.supplyAsync(() -> {
+            return helloService.insert1();
+        });
+        CompletableFuture<String> fu3 = CompletableFuture.supplyAsync(() -> {
+            return helloService.insert2();
+        });
+        CompletableFuture<String> fu4 = CompletableFuture.supplyAsync(() -> {
+            return helloService.insert3();
+        });
+        CompletableFuture.allOf(fu1, fu2, fu3, fu4).join();
+        System.out.println(Thread.currentThread().getName() + "insertData time:" + (System.currentTimeMillis() - begin));
+
         System.out.println(Thread.currentThread().getName() + " --------------end---" + planNo);
     }
 
