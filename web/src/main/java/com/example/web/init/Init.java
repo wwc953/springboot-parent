8e190860761253912914395fb3d32c293e0425f5
wangwc <wwc953@163.com>
2020-09-11 14:09:54 +0800

    rename

--- web/src/main/java/com/example/web/init/Init.java
+++ web/src/main/java/com/example/web/init/Init.java
@@ -1,8 +1,6 @@
-package com.example.web.event;
+package com.example.web.init;
 
-import com.example.web.service.TaskService;
 import com.example.web.threadfactory.RedisThreadFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -11,25 +9,21 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Description: TODO
+ * @Description:
  * @author: wangwc
  * @date: 2020/9/8 14:57
  */
 @Component
 public class Init implements ApplicationListener<ApplicationReadyEvent> {
 
-    //    @Autowired
-//    TaskService taskService;
     private int corePoolsize = 5;
     ScheduledThreadPoolExecutor scheduledThreadPoolExecutor =
             new ScheduledThreadPoolExecutor(corePoolsize, new RedisThreadFactory("redisRead"));
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-//        taskService.init();
-//        System.out.println("init ok");
         for (int i = 1; i <= 5; i++) {
-            int finalI = i;
+
             scheduledThreadPoolExecutor.scheduleWithFixedDelay(new Runnable() {
                 @Override
                 public void run() {
