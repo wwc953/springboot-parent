0cfd49815f3c3b58dc3b864cd78b3df0b1c3059f
wangwc <wwc953@163.com>
2020-09-17 16:46:50 +0800

    redis ChannelTopic

--- web/src/main/java/com/example/web/controller/HelloController.java
+++ web/src/main/java/com/example/web/controller/HelloController.java
@@ -1,10 +1,9 @@
 package com.example.web.controller;
 
-import com.example.web.bean.User;
 import com.example.service.IHelloService;
+import com.example.web.bean.User;
 import com.example.web.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.web.bind.annotation.*;
