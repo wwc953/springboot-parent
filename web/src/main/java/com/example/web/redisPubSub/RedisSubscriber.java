89d5b6459ddceb556e78d55f2ebc0028def1a695
wangwc <wwc953@163.com>
2020-10-15 15:58:33 +0800

    Redis消息监听应用场景(刷新本地缓)

--- web/src/main/java/com/example/web/redisPubSub/RedisSubscriber.java
+++ web/src/main/java/com/example/web/redisPubSub/RedisSubscriber.java
@@ -4,11 +4,16 @@ import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.stereotype.Component;
 
+/**
+ * Redis消息监听 (可用于刷新本地缓)
+ */
 @Component
 public class RedisSubscriber extends MessageListenerAdapter {
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
+        System.out.println("刷新本地缓存开始...");
         System.out.println("msg:" + message + "，pattern：" + new String(pattern));
+        System.out.println("刷新本地缓存结束...");
     }
 }
